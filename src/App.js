0676b0f5b233d4d6445492075d30a2ac6d0c5d4b
김동균[Data&Machine Learning Lab] <gyun.kimdong@navercorp.com>
2021-01-28 10:25:53 +0900

    first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          배포가 잘 되었을까용
         </p>
         <a
           className="App-link"
