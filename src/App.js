e57de7e846b7b5a1a79d79f31d1c877c76157005
김동균[Data&Machine Learning Lab] <gyun.kimdong@navercorp.com>
2021-01-28 18:17:55 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ci/cd가 잘 되었을까용..
+          ci/cd가 잘 되었을까용..?
         </p>
         <a
           className="App-link"
